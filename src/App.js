754c6994a0199e1584aa9fd1eb0696425e5bb79f
nlerner <noahlerner94@gmail.com>
2020-01-12 15:16:37 +0200

    Add log in component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
-import './App.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.css';
 import {BrowserRouter as Router, Switch, Route, Link} from 'react-router-dom';
 
 import Login from './Components/login.component';
@@ -11,14 +10,16 @@ function App() {
     <div className="App">
       <nav className="navbar navbar-expand-lg navbar-light fixed-top">
         <div className="container">
-          <ul className="navbar-nav ml-auto">
-            <li className="nav-item">
-              <Link className="nav-link" to={"/sign-in"}>Login</Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to={"/sign-up"}>Sign up</Link>
-            </li>
-          </ul>
+          <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
+            <ul className="navbar-nav ml-auto">
+              <li className="nav-item">
+                <Link className="nav-link" to={"/sign-in"}>Login</Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to={"/sign-up"}>Sign up</Link>
+              </li>
+            </ul>   
+          </div>
         </div>
       </nav>
     </div>
--- src/Components/login.component.js
+++ src/Components/login.component.js
@@ -1,11 +1,34 @@
 import React, { Component } from 'react';
+import '../../node_modules/bootstrap/dist/css/bootstrap.css';
 
 export default class Login extends Component {
     render() {
         return (
-            <div>
-                <h3>React Login Component</h3>
-            </div>
+            <form>
+                <h3>Sign in</h3>
+
+                <div className="form-group">
+                    <label>Email address</label>
+                    <input type="email" className="form-control" placeholder="yourmail@host.com" />
+                </div>
+
+                <div className="form-group">
+                    <label>Password</label>
+                    <input type="password" className="form-control" placeholder="password" />
+                </div>
+
+                <div className="form-group">
+                    <div className="custom-control custom-checkbox">
+                        <input type="checkbox" className="custom-control-input" id="customCheck1" />
+                        <label className="custom-control-label" htmlFor="customCheck1">Remember me</label>
+                    </div>
+                </div>
+
+                <button type="submit" className="btn btn-primary btn-block">Submit</button>
+                <p className="forgot-password text-right">
+                    Forgot password?
+                </p>
+            </form>
         );
     }
 }
