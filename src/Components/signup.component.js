7fc108b96669f9aad8072c85f530c72509531755
Noah Lerner <noah.lerner@verint.com>
2020-01-12 13:26:17 +0200

    Login and Signup initial commit

---
+++ src/Components/signup.component.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+export default class SignUp extends Component {
+    render() {
+        return(
+            <div>
+                <h3>React Signup Component</h3>
+            </div>
+        );
+    }
+}
